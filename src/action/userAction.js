e293cf1feadee726731a0daf306f350d0b791106
NiladriModak <niladrimodakk2001@gmail.com>
2023-11-22 23:36:36 +0530

    front1

--- src/action/userAction.js
+++ src/action/userAction.js
@@ -30,7 +30,7 @@ export const login=(email,password)=>async(dispatch)=>{
     try {
         dispatch({type:LOGIN_REQUEST});
         const config={header:{"Content-Type":"application.json"}}
-        const {data} = await axios.post("https://ad-backend-1dvu.onrender.com/login",{email,password},config);
+        const {data} = await axios.post("https://adhub.onrender.com/login",{email,password},config);
         dispatch({type:LOGIN_SUCCESS,payload:data.user});
     } catch (error) {
         dispatch({type:LOGIN_FAIL,payload:error.response.data.message})
@@ -44,7 +44,7 @@ export const register = (userData) => async (dispatch) => {
   
       const config = { headers: { "Content-Type": "multipart/form-data" } };
   
-      const { data } = await axios.post(`https://ad-backend-1dvu.onrender.com/register`, userData, config);
+      const { data } = await axios.post(`https://adhub.onrender.com/register`, userData, config);
   
       dispatch({ type: REGISTER_SUCCESS, payload: data.user });
     } catch (error) {
@@ -58,7 +58,7 @@ export const register = (userData) => async (dispatch) => {
 export const loadUser=()=>async(dispatch)=>{
     try {
         dispatch({type:LOAD_USER_REQUEST})
-        const {data} = await axios.get("https://ad-backend-1dvu.onrender.com/me");
+        const {data} = await axios.get("https://adhub.onrender.com/me");
         dispatch({type:LOAD_USER_SUCCESS,payload:data.user})
     } catch (error) {
         dispatch({type:LOAD_USER_FAIL,
@@ -68,7 +68,7 @@ export const loadUser=()=>async(dispatch)=>{
 
 export const logout=()=>async(dispatch)=>{
     try {
-        await axios.get("https://ad-backend-1dvu.onrender.com/logout");
+        await axios.get("https://adhub.onrender.com/logout");
         dispatch({type:LOGOUT_SUCCESS})
     } catch (error) {
         dispatch({type:LOGOUT_FAIL,
@@ -81,7 +81,7 @@ export const logout=()=>async(dispatch)=>{
 export const getAllUsers = () => async (dispatch) => {
     try {
       dispatch({ type: ALL_USERS_REQUEST });
-      const { data } = await axios.get(`https://ad-backend-1dvu.onrender.com/admin/users`);
+      const { data } = await axios.get(`https://adhub.onrender.com/admin/users`);
   
       dispatch({ type: ALL_USERS_SUCCESS, payload: data.users });
     } catch (error) {
@@ -93,7 +93,7 @@ export const getAllUsers = () => async (dispatch) => {
   export const getUserDetails = (id) => async (dispatch) => {
     try {
       dispatch({ type: USER_DETAILS_REQUEST });
-      const { data } = await axios.get(`https://ad-backend-1dvu.onrender.com/admin/users/${id}`);
+      const { data } = await axios.get(`https://adhub.onrender.com/admin/users/${id}`);
   
       dispatch({ type: USER_DETAILS_SUCCESS, payload: data.user });
     } catch (error) {
@@ -110,7 +110,7 @@ export const updateUser = (id, userData) => async (dispatch) => {
       const config = { headers: { "Content-Type": "application/json" } };
   
       const { data } = await axios.put(
-        `https://ad-backend-1dvu.onrender.com/admin/users/${id}`,
+        `https://adhub.onrender.com/admin/users/${id}`,
         userData,
         config
       );
@@ -129,7 +129,7 @@ export const updateUser = (id, userData) => async (dispatch) => {
     try {
       dispatch({ type: DELETE_USER_REQUEST });
   
-      const { data } = await axios.delete(`https://ad-backend-1dvu.onrender.com/admin/users/${id}`);
+      const { data } = await axios.delete(`https://adhub.onrender.com/admin/users/${id}`);
   
       dispatch({ type: DELETE_USER_SUCCESS, payload: data });
     } catch (error) {
